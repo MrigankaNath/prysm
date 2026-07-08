db9f11931edb7c629c64713ac11c2481e6b90c14
Mriganka <64747515+MrigankaNath@users.noreply.github.com>
2026-07-08 11:24:09 +0530

    feat: add initial content item data model

---
+++ data/contentItems.js
@@ -0,0 +1,18 @@
+const contentItems = [
+  {
+    id: 1,
+    title: "The Case for Space",
+    type: "podcast",
+    topic: "astrophysics",
+    url: "https://example.com/case-for-space",
+  },
+  {
+    id: 2,
+    title: "Why We Don't See Aliens",
+    type: "video",
+    topic: "astrophysics",
+    url: "https://example.com/aliens",
+  },
+];
+
+module.exports = contentItems;
