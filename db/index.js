cb2a4d7aa44ec2bf1a59bc24da4ed8a0058a700a
Mriganka <64747515+MrigankaNath@users.noreply.github.com>
2026-08-05 13:58:07 +0530

    feat: connect to Supabase Postgres

--- db/index.js
+++ db/index.js
@@ -1,10 +1,8 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.USER,
-  host: "localhost",
-  database: "prysm_dev",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
